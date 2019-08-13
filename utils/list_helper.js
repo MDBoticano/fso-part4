037d6be74ec72556254b635f0369fc4b9b170d98
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-13 15:29:30 -0700

    4.5 Bloglist helper func and unit tests step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  const sumReducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.reduce(sumReducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
