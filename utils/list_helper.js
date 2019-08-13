370bba116b0ff4c2b642ac5df68cf59e54e80115
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-13 15:54:41 -0700

    4.5 Bloglist helper functions and unit tests step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,28 @@ const totalLikes = (blogs) => {
   return blogs.reduce(sumReducer, 0)
 }
 
+
+/* Returns the favorite blog (one with most likes) */
+const favoriteBlog = (blogs) => {
+  let favorite = {}
+  
+  const favoriteReducer = (mostLikes, blog) => {
+    if (blog.likes > mostLikes) {
+      /* Change mostLikes to be the current favorite blog's likes */
+      mostLikes = blog.likes
+      /* If blog's likes is more than mostLikes, put it as favorite */
+      favorite = blog
+    }
+    return mostLikes
+  }
+
+  blogs.reduce(favoriteReducer, -1)
+
+  return favorite  
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
