b5c0dbfae96747e48cc968bd1bb174ea2395d859
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-15 17:47:58 -0700

    4.13 delete by ID works

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -127,6 +127,22 @@ test('a specific blog can be viewed', async () => {
   expect(resultBlog.body).toEqual(blogToView)
 })
 
+/* 4.13: a specific blog can be deleted */
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd.length).toBe(helper.sampleBlogs.length - 1)
+
+  const titles = blogsAtEnd.map(blog => blog.title)
+  expect(titles).not.toContain(blogToDelete.title)
+})
+
 
 /* -------------------- After all tests, close connection ------------------- */
 afterAll(() => {
