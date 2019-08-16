166ac7a857fa3957e2512068a53fa2dffe0d1759
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-15 17:35:41 -0700

    Added GET by id and DELETE by id methods (untested)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,12 +1,27 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-/* GET: all blogs */ /* ASYNC */
+/* GET: all blogs -- ASYNC */
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
+/* GET: one blog using id --  */
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(note.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+/* POST: create a new entry -- ASYNC */
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
@@ -20,6 +35,16 @@ blogsRouter.post('/', async (request, response, next) => {
   try {
     const savedBlog = await blog.save()
     response.json(savedBlog.toJSON())
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+/* DELETE: one blog using id -- ASYNC */
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
   } catch (exception) {
     next(exception)
   }
