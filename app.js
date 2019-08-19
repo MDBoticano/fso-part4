e2d2a6ffa6ccd4531016cfb4780e7270ef193967
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-19 16:38:06 -0700

    4.15 Bloglist (create user by POST; other files)

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
+
+// Enable depending on Mongoose deprecation warnings
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
@@ -21,6 +26,7 @@ app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs/', blogsRouter)
+app.use('/api/users/', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
