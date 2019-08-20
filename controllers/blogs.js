5c80da81a5312432686ef7adfc00344b206283ce
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-20 16:20:22 -0700

    4.20 Bloglist: created tokenExtractor middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,16 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-/* Helper for Authentication */
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    // Return the rest of the string after 'bearer '
-    return authorization.substring(7)
-  }
-  return null
-}
-
 /* GET: all blogs -- ASYNC */
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -40,21 +30,18 @@ blogsRouter.get('/:id', async (request, response, next) => {
 /* POST: create a new entry -- ASYNC */
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const token = getTokenFrom(request)
 
   try {
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    // console.log('decoded token', decodedToken.id)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    
+    if (!request.token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
   
     const user = await User.findById(body.userId)
 
     // validate who the token belongs to, not just that its valid
-    // console.log('user id', user._id.toString())
     if(decodedToken.id.toString() !== user._id.toString()) {
-      // console.log('token for wrong user')
       return response.status(401).json({ error: 'token doesn\'t match user' })
     }
 
