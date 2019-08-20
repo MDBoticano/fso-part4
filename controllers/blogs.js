9b090113172c95fff4f3eb7446ca9ee9c3a25f3a
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-20 15:25:50 -0700

    4.18 Bloglist: unintended behavior, can submit blog as any user as long as token is valid

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+/* Helper for Authentication */
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    // Return the rest of the string after 'bearer '
+    return authorization.substring(7)
+  }
+  return null
+}
 
 /* GET: all blogs -- ASYNC */
 blogsRouter.get('/', async (request, response) => {
@@ -29,17 +40,24 @@ blogsRouter.get('/:id', async (request, response, next) => {
 /* POST: create a new entry -- ASYNC */
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const user = await User.findById(body.userId)
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes === undefined ? 0 : body.likes,
-    user: user._id
-  })
+  const token = getTokenFrom(request)
 
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+  
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes === undefined ? 0 : body.likes,
+      user: user._id
+    })
+  
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
